ddd37b69b6319fcfdbbe224692c34bb10d9002de
Javier Guerrero <javier@bertonisolutions.com>
2023-04-21 09:25:17 -0500

    questions for animals category

--- Infrastructure/DataAccess/Repositories/QuestionRepository.cs
+++ Infrastructure/DataAccess/Repositories/QuestionRepository.cs
@@ -24,7 +24,19 @@ namespace Infrastructure.DataAccess.Repositories
                 new Question() { Id = 12, CategoryId = 1, Text = "What is a good age to 'settle down' and get married?" },
                 new Question() { Id = 13, CategoryId = 1, Text = "Should younger people respect the elderly? Why or why not?" },
                 new Question() { Id = 14, CategoryId = 1, Text = "Where you're from, is it okay to ask people about their age?" },
-                new Question() { Id = 15, CategoryId = 1, Text = "Should job applicants be required to disclose their age to potential employers?" }
+                new Question() { Id = 15, CategoryId = 1, Text = "Should job applicants be required to disclose their age to potential employers?" },
+
+                new Question() { Id = 16, CategoryId = 2, Text = "Do you like animals? Which ones?" },
+                new Question() { Id = 17, CategoryId = 2, Text = "Do you have any pets?" },
+                new Question() { Id = 18, CategoryId = 2, Text = "Did you have any pets when you were younger?" },
+                new Question() { Id = 19, CategoryId = 2, Text = "Some people spend thousands of dollars on their pets, especially when they get sick. What are your thoughts about this?" },
+                new Question() { Id = 20, CategoryId = 2, Text = "Do you have a favorite animal?" },
+                new Question() { Id = 21, CategoryId = 2, Text = "Are you afraid of any animals?" },
+                new Question() { Id = 22, CategoryId = 2, Text = "Why do you think so many people are scared of sharks? Should they be scared of them?" },
+                new Question() { Id = 23, CategoryId = 2, Text = "Have you seen any movies or read any books lately that featured animals in them?" },
+                new Question() { Id = 24, CategoryId = 2, Text = "What do you think about zoos?" },
+                new Question() { Id = 25, CategoryId = 2, Text = "What are your thoughts about eating meat raised on factory farms?" },
+                new Question() { Id = 26, CategoryId = 2, Text = "Should people be allowed to eat wild animals like snakes, whales, etc.?" }
             };
         }
 
