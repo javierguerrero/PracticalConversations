1bf955b4b2edb7d512bc403464ba29b6503e6035
Javier Guerrero <javier@bertonisolutions.com>
2023-04-21 00:32:17 -0500

    questions for "age and aging" category

--- Infrastructure/DataAccess/Repositories/QuestionRepository.cs
+++ Infrastructure/DataAccess/Repositories/QuestionRepository.cs
@@ -10,10 +10,21 @@ namespace Infrastructure.DataAccess.Repositories
         public QuestionRepository()
         {
             _questions = new List<Question>() {
-                new Question() { Id = 1, Text = "filosfia", CategoryId = 1 },
-                new Question() { Id = 2, Text = "historia", CategoryId = 1 },
-                new Question() { Id = 3, Text = "música", CategoryId = 2 },
-                new Question() { Id = 4, Text = "teatro", CategoryId = 2 },
+                new Question() { Id = 1, CategoryId = 1, Text = "What is the best age to be? Why?" },
+                new Question() { Id = 2, CategoryId = 1, Text = "What's been your favorite age so far in your life? Why?" },
+                new Question() { Id = 3, CategoryId = 1, Text = "Do you have any fears about getting older? What are they?" },
+                new Question() { Id = 4, CategoryId = 1, Text = "Who is the oldest person that you know? Are you close to him/her?" },
+                new Question() { Id = 5, CategoryId = 1, Text = "Is age important in your culture?" },
+                new Question() { Id = 6, CategoryId = 1, Text = "Can people of different ages be friends?" },
+                new Question() { Id = 7, CategoryId = 1, Text = "Are you friends with someone much older or younger than you?" },
+                new Question() { Id = 8, CategoryId = 1, Text = "What are some of your earliest memories?" },
+                new Question() { Id = 9, CategoryId = 1, Text = "What was the hardest part about growing up for you?" },
+                new Question() { Id = 10, CategoryId = 1, Text = "What advice would you give to your younger self?" },
+                new Question() { Id = 11, CategoryId = 1, Text = "What do you think of nursing homes? Are they a nice option for older people or should they live with family members?" },
+                new Question() { Id = 12, CategoryId = 1, Text = "What is a good age to 'settle down' and get married?" },
+                new Question() { Id = 13, CategoryId = 1, Text = "Should younger people respect the elderly? Why or why not?" },
+                new Question() { Id = 14, CategoryId = 1, Text = "Where you're from, is it okay to ask people about their age?" },
+                new Question() { Id = 15, CategoryId = 1, Text = "Should job applicants be required to disclose their age to potential employers?" }
             };
         }
 
@@ -24,7 +35,7 @@ namespace Infrastructure.DataAccess.Repositories
                 .ToList();
         }
 
-        public  Question GetQuestionById(int id)
+        public Question GetQuestionById(int id)
         {
             return _questions.Single(q => q.Id == id);
         }
