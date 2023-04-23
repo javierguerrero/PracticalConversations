4a2f3ce7fb395c47abb17ad06bb01af75251297a
Javier Guerrero <javier@bertonisolutions.com>
2023-04-23 14:47:35 -0500

    submit validation, category required

--- src/WebUI/ViewModels/HomeViewModel.cs
+++ src/WebUI/ViewModels/HomeViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebUI.ViewModels
 {
     public class HomeViewModel
     {
+        [Required(ErrorMessage = "Category is required.")]
+
         public string SelectedCategory { get; set; }
         public List<SelectListItem> CategoryList { get; set; } = new List<SelectListItem>();
 
